af62aa1234244811c238584e273ceeb97106fc93
cuneytinann <cuneytinan37@gmail.com>
2026-05-03 13:48:43 +0300

    o

--- push.bat
+++ push.bat
@@ -1,14 +1,14 @@
 @echo off
 cd /d "C:\Users\Hp\OneDrive\Desktop\zincirikirma"
-
+ 
 git add .
-
+ 
 set /p msg="Commit mesaji: "
-
+ 
 git commit -m "%msg%"
-
-git push origin master
-
+ 
+git push origin main
+ 
 echo.
 echo Push tamamlandi! Vercel deploy ediliyor...
 echo https://zincirikirma.org
